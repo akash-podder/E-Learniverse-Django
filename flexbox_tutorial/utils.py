857a9d5c58a4403709d4534706435d49c95a2d86
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-05-18 20:22:37 +0600

    -  Vertical Flexbox Codes are added in `FlexboxTutorialsUtils` class

--- flexbox_tutorial/utils.py
+++ flexbox_tutorial/utils.py
@@ -1,6 +1,8 @@
 class FlexboxTutorialsUtils:
 
     dic = {
+
+        # HORIZONTAL Tutorials
         "Default-Flexbox":"""<div class="d-flex bg-dark">
             \n<div class="text-white p-2 bg-success">Item 1</div>
             \n<div class="text-white p-2 bg-primary">Item 2</div>
@@ -31,6 +33,56 @@ class FlexboxTutorialsUtils:
             \n<div class="text-white p-2 bg-danger order-2">Item 3</div>
         \n</div>""",
 
+
+
+        # VERTICAL Tutorials
+        "Justify-Content-Start": """<div class="d-flex justify-content-start bg-dark" style="height: 100px;">
+            \n<div class="text-white p-2 bg-success">Item 1</div>
+            \n<div class="text-white p-2 bg-primary">Item 2</div>
+            \n<div class="text-white p-2 bg-danger">Item 3</div>
+        </div>""",
+
+        "Align-Items-Start": """<div class="d-flex align-items-start bg-dark" style="height: 100px;">
+            \n<div class="text-white p-2 bg-success">Item 1</div>
+            \n<div class="text-white p-2 bg-primary">Item 2</div>
+            \n<div class="text-white p-2 bg-danger">Item 3</div>
+        </div>""",
+
+        "Align-Items-End": """<div class="d-flex align-items-end bg-dark" style="height: 100px;">
+                \n<div class="text-white p-2 bg-success">Item 1</div>
+                \n<div class="text-white p-2 bg-primary">Item 2</div>
+                \n<div class="text-white p-2 bg-danger">Item 3</div>
+            </div>""",
+
+        "Align-Items-Center": """<div class="d-flex align-items-center bg-dark" style="height: 100px;">
+                    \n<div class="text-white p-2 bg-success">Item 1</div>
+                    \n<div class="text-white p-2 bg-primary">Item 2</div>
+                    \n<div class="text-white p-2 bg-danger">Item 3</div>
+                </div>""",
+
+        "Align-Items-Center-Justy-Content-Center": """<div class="d-flex align-items-center justify-content-center bg-dark" style="height: 100px;">
+            \n<div class="text-white p-2 bg-success">Item 1</div>
+            \n<div class="text-white p-2 bg-primary">Item 2</div>
+            \n<div class="text-white p-2 bg-danger">Item 3</div>
+        </div>""",
+
+        "Justy-Content-No-Wrap": """<div class="d-flex justify-content-around flex-nowrap bg-dark">
+            \n<div class="text-white p-2 bg-success">Item 1</div>
+            \n<div class="text-white p-2 bg-primary">Item 2</div>
+            \n<div class="text-white p-2 bg-danger">Item 3</div>
+            \n<div class="text-white p-2 bg-success">Item 1</div>
+            \n<div class="text-white p-2 bg-primary">Item 2</div>
+            \n<div class="text-white p-2 bg-danger">Item 3</div>
+        </div>""",
+
+        "Justy-Content-Wrap": """<div class="d-flex justify-content-around flex-wrap bg-dark">
+                \n<div class="text-white p-2 bg-success">Item 1</div>
+                \n<div class="text-white p-2 bg-primary">Item 2</div>
+                \n<div class="text-white p-2 bg-danger">Item 3</div>
+                \n<div class="text-white p-2 bg-success">Item 1</div>
+                \n<div class="text-white p-2 bg-primary">Item 2</div>
+                \n<div class="text-white p-2 bg-danger">Item 3</div>
+            </div>""",
     }
 
     def getCodeFromFlexboxTutorialName(self, tutorialName):
