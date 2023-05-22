54542b5b5a47b5276ff5c96bcfa309aa37210686
AnantaAkashPodder <anantaakash.podder@gmail.com>
2023-05-22 19:53:32 +0600

    - `learn_flexbox_tutorial` app views Converted into CLASS-BASED view

--- flexbox_tutorial/urls.py
+++ flexbox_tutorial/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('horizontal', views.learn_flexbox_horizontal, name = "learn_flexbox_horizontal"),
-    path('vertical', views.learn_flexbox_vertical, name = "learn_flexbox_vertical"),
-    path('code/<str:code_id>', views.ShowFlexboxCodeView.as_view(), name='show_flexbox_code'),
+    path('horizontal', views.HorizontalTutorialView.as_view(), name = views.HorizontalTutorialView.view_name),
+    path('vertical', views.VerticalTutorialTemplateView.as_view(), name = views.VerticalTutorialTemplateView.view_name),
+    path('code/<str:code_id>', views.ShowFlexboxCodeView.as_view(), name=views.ShowFlexboxCodeView.view_name),
 ]
--- flexbox_tutorial/views.py
+++ flexbox_tutorial/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 from .utils import FlexboxTutorialsUtils
 
+# extends Normal Class-based `View`
+class HorizontalTutorialView(View):
+    view_name = 'learn_flexbox_horizontal'
+    def get(self, request):
+        return render(request, 'flexbox_tutorial/learn-flexbox-horizontal.html')
 
-def learn_flexbox_horizontal(request):
-    return render(request, 'flexbox_tutorial/learn-flexbox-horizontal.html')
+# extends class-based `TemplateView`
+class VerticalTutorialTemplateView(TemplateView):
+    view_name = 'learn_flexbox_vertical'
+    template_name = "flexbox_tutorial/learn-flexbox-vertical.html"
 
-def learn_flexbox_vertical(request):
-    return render(request, 'flexbox_tutorial/learn-flexbox-vertical.html')
 
-class ShowFlexboxCodeView(View):
+# By default, the TemplateView class in Django is intended for handling GET requests and rendering templates.
+# It does not provide built-in support for handling POST requests.
+class ShowFlexboxCodeView(TemplateView):
     view_name = 'show_flexbox_code'
-    def get(self, request, code_id):
+    template_name = "flexbox_tutorial/show-flexbox-code.html"
+
+    def get_context_data(self, **kwargs):
+        # we call the PARENT CLASS method(super().get_context_data(**kwargs)) to get the default context data.
+        context = super().get_context_data(**kwargs)
+
         flexbox_tutorial_utils = FlexboxTutorialsUtils()
+        code_id = self.kwargs.get('code_id')
         code_str = flexbox_tutorial_utils.getCodeFromFlexboxTutorialName(code_id)
 
-        context = {'code_str': code_str}
-        return render(request, 'flexbox_tutorial/show-flexbox-code.html', context)
+        context['code_str'] = code_str
+
+        return context
