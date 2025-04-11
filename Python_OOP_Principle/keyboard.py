06e124c546a8a259fe3bb5a81dc7d93cc378ae8e
AkashPodder <anantaakash.podder@gmail.com>
2025-04-11 19:08:45 +0600

    - `Python_OOP_Principle`: added some documentation & renamed a file for better understanding

--- Python_OOP_Principle/keyboard.py
+++ Python_OOP_Principle/keyboard.py
@@ -3,6 +3,9 @@ from item import Item
 
 class Keyboard(Item):
     pay_rate = 0.7
+
+    # "__" --> this is called Dunder
+    # __init__ --> this is the Constructor of any Class
     def __init__(self, name: str, price: float, quantity=0):
         # Call to super function to have access to all attributes / methods
         super().__init__(
