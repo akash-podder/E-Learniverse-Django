766fbca855aaffd5b03cb2e3905d12becb09dcb4
AkashPodder <anantaakash.podder@gmail.com>
2025-05-19 16:58:08 +0600

    - python `.join` Operator Example added

---
+++ python_tutorial/python_JOIN_operator/python_JOIN_example.py
@@ -0,0 +1,13 @@
+words = [
+    "Sergio",
+    "Ramos",
+    "says",
+    "Hola",
+    "Amigos"
+]
+
+# Joins each word in the list into a single string
+# Puts a space ' ' between each word
+sentence = ' '.join(words)
+
+print(sentence)
