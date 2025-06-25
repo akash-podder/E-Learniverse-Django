fb8f186ad159ed300869d29333fbe324fc1265ea
AkashPodder <anantaakash.podder@gmail.com>
2025-06-25 14:28:58 +0600

    - added `delete_all_node_with_given_value` in `Linked List` of `python_data_structure_for_competitive_programming` folder

--- python_tutorial/python_data_structure_for_competitive_programming/linked_list.py
+++ python_tutorial/python_data_structure_for_competitive_programming/linked_list.py
@@ -34,8 +34,33 @@ class MyLinkedList:
                 break
 
             prev = current
+            # updating "current" for Next Iteration
             current = current.next
 
+        # Update the root in case head was deleted
+        self.root = dummy.next
+
+    def delete_all_node_with_given_value(self, val_to_be_deleted: int):
+        dummy = ListNode()
+        dummy.next = self.root
+
+        prev = dummy
+        current = self.root
+
+        while current is not None:
+            if current.val == val_to_be_deleted:
+                prev.next = current.next
+
+            # This is the Difference Between Deleting 1 Node VS All Nodes with Same Values
+            else:
+                # prev = current only advances when the node is not deleted — this avoids skipping nodes.
+                # Move prev only if current node is not deleted
+                prev = current
+
+            # updating "current" for Next Iteration
+            current = current.next
+
+        # Update the root in case head was deleted
         self.root = dummy.next
 
     def reverse_linked_list(self):
@@ -67,15 +92,20 @@ class MyLinkedList:
         print("")
 
 if __name__ == "__main__":
-    my_list = MyLinkedList(2)
+    my_list = MyLinkedList(51)
+    my_list.add_node(2)
     my_list.add_node(1)
     my_list.add_node(55)
+    my_list.add_node(51)
     my_list.add_node(58)
     my_list.add_node(6)
     my_list.add_node(51)
     my_list.print_linked_list()
 
     my_list.delete_node(58)
+    my_list.print_linked_list()
+
+    my_list.delete_all_node_with_given_value(51)
     my_list.print_linked_list()
 
     my_list.reverse_linked_list()
